7045ead31df0ae084a43e722a6f74cd795187543
Bong <qhdgkdbs@gmail.com>
2023-01-31 16:54:57 +0900

    ✨ 서브 패키지 a 추가

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const dayjs = require('dayjs');
+const a = require('a');
 
-console.log(dayjs().format());
+console.log(`Today's date: ${dayjs().format()}`);
+console.log(`From package a: ${a.getMessage()}`);
