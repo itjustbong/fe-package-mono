f13a970aa398691ae1aca99c0a21bcfacf564013
Bong <qhdgkdbs@gmail.com>
2023-02-01 15:26:20 +0900

    ✨ shared ui, app client 모노레포 합성

--- fe-mono/packages/shared-ui/Button.jsx
+++
@@ -1,4 +0,0 @@
-export function Button(props) {
-  return <button onClick={() => props.onClick()}>{props.children}</button>;
-}
-export default Button;
---
+++ fe-mono/packages/shared-ui/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import type { FC, PropsWithChildren } from 'react';
+
+interface Props extends PropsWithChildren {}
+
+const Button: FC<Props> = ({ children }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
