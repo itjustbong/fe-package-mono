36e0b54893817c2e1b1d7202222cc1173ebf8077
Bong <qhdgkdbs@gmail.com>
2023-02-01 23:26:02 +0900

    📝 컴포넌트 스토리에 대한 설명 주석 추가

--- storybook/src/stories/Hello/Hello.stories.tsx
+++ storybook/src/stories/Hello/Hello.stories.tsx
@@ -1,26 +1,27 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React from 'react';
 import Hello from './Hello';
-import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 export default {
   title: 'Ssung/Hello', // 스토리북에서 보여질 그룹과 경로를 명시
   component: Hello, // 어떤 컴포넌트를 문서화 할지 명시
-  decorators: [withKnobs], // 애드온 적용
+  // decorators: [withKnobs], // 애드온 적용
+  // Docs 탭에서 컴포넌트에 대한 부제목을 추가하는 옵션
+  parameters: {
+    componentSubtitle: '"안녕하세요"라고 보여주는 컴포넌트',
+  },
 } as ComponentMeta<typeof Hello>;
 
 const Template: ComponentStory<typeof Hello> = (args) => <Hello {...args} />;
 
-// 컴포넌트 props 넘기는 법 2가지
+// 컴포넌트 props 넘기는 법 2가지.
+// 하기와 같이 하면 전달하는 인자 값을 변경해보면서,
+// 테스트 해볼 수 있음.
 export const HelloWithArgs = Template.bind({});
 HelloWithArgs.args = {
   name: 'hello',
 };
 
-// @storybook/addon-knobs 를 통해 각종 타입들의 props들이
-// 이벤트에 반응하여 변경될 수 있음
 export const HelloWithComponent = () => {
-  const big = boolean('big', false);
-  const name = text('name', 'Storybook');
-  return <Hello name={name} big={big} />;
+  return <Hello name={'이름'} big />;
 };
--- storybook/src/stories/Hello/Hello.tsx
+++ storybook/src/stories/Hello/Hello.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 interface HelloProps {
+  /** 이름 */
   name: string;
+  /** 타이틀 크게 */
   big?: boolean;
+  /** 이렇게 주석을 달면 Props의 Description에서 보여집니다 */
   onHello?: () => void;
 }
 
-const Hello = ({ name, big, onHello }: HelloProps) => {
+// 컴포넌트 바로 위 주석은 Docs에서 컴포넌트 상단에 추가되는 설명
+/**
+ * 안녕하세요, 쓩 딜리버리 입니다 라는 컴포넌트입니다.
+ * - props 값으로 name, big, onHello 를 인자로 받습니다.
+ */
+const Hello = ({ name, big = false, onHello }: HelloProps) => {
   if (big) {
     return <h1 onClick={onHello}>안녕하세요, 쓩 딜리버리 입니다! {name}!</h1>;
   }
